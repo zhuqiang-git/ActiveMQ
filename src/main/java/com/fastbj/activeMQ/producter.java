3bdc2921ca258aa3ab0559cb82fe946e1e849152
zhuqiang-git <16562083@qq.com>
2020-03-08 22:37:36 +0800

    完成消息队列

--- src/main/java/com/fastbj/activeMQ/producter.java
+++ src/main/java/com/fastbj/activeMQ/producter.java
@@ -10,33 +10,41 @@ import javax.jms.*;
 public class producter {
 
     public static void main(String[] args) throws JMSException {
-        System.out.println("Hello java");
-//        Connectionfactory
+        // ConnectionFactory ：连接工厂，JMS 用它创建连接
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
                 ActiveMQConnection.DEFAULT_USER,
                 ActiveMQConnection.DEFAULT_PASSWORD,
                 "tcp://118.25.154.234:61616");
+        // JMS 客户端到JMS Provider 的连接
         Connection connection = connectionFactory.createConnection();
         connection.start();
+        // Session： 一个发送或接收消息的线程
         Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+        // Destination ：消息的目的地;消息发送给谁.
+        // 获取session注意参数值my-queue是Query的名字
         Destination destination = session.createQueue("my-queue");
+        // MessageProducer：消息生产者
         MessageProducer producer = session.createProducer(destination);
+        // 设置不持久化（就是设置为点对点通信，如果持久化就是发布订阅模式）
         producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        for (int i = 1; i <= 5; i++) {
+        // 发送一条消息
+        for (int i = 1; i <= 500000; i++) {
             sendMsg(session, producer, i);
         }
         connection.close();
     }
 
     /**
-     * 向ActiveMQ发送消息
+     * 在指定的会话上，通过指定的消息生产者发出一条消息
      *
-     * @param session
-     * @param producer
+     * @param session 消息会话
+     * @param producer  消息生产者
      * @param i
      */
     private static void sendMsg(Session session, MessageProducer producer, int i) throws JMSException {
+        // 创建一条文本消息
         TextMessage message = session.createTextMessage("Hello ActiveMQ! " + i);
+        // 通过消息生产者发出消息
         producer.send(message);
     }
 
