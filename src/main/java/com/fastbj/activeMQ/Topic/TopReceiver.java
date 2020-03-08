312ee7e8bf2c0e28a7c961cbb75c338cc1e1d663
zhuqiang-git <16562083@qq.com>
2020-03-08 23:39:27 +0800

    修改发布订阅模式

--- src/main/java/com/fastbj/activeMQ/Topic/TopReceiver.java
+++ src/main/java/com/fastbj/activeMQ/Topic/TopReceiver.java
@@ -14,7 +14,7 @@ public class TopReceiver {
     }
 
     private static void start() throws JMSException {
-        System.out.println("消费者启动......");
+        System.out.println("消费点启动...");
         // 创建ActiveMQConnectionFactory 会话工厂
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
                 ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKERURL);
@@ -22,15 +22,15 @@ public class TopReceiver {
         // 启动JMS 连接
         connection.start();
         // 不开消息启事物，消息主要发送消费者,则表示消息已经签收
-        Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         // 创建一个队列
         Topic topic = session.createTopic(TOPIC);
         MessageConsumer consumer = session.createConsumer(topic);
         // consumer.setMessageListener(new MsgListener());
         while (true) {
             TextMessage textMessage = (TextMessage) consumer.receive();
-            if (null != textMessage) {
-                System.out.println("接收到消息：" + textMessage.getText());
+            if (textMessage != null) {
+                System.out.println("接受到消息:" + textMessage.getText());
                 // textMessage.acknowledge();// 手动签收
                 // session.commit();
             } else {
