062dc19dbec5785b17b58ac195893c48d3b976d8
qiang.zhu <16562083@qq.com>
2022-09-27 19:27:34 +0800

    修改超时时间配置

--- src/main/java/com/fastbj/activeMQ/enums/RegistryConfig.java
+++ src/main/java/com/fastbj/activeMQ/enums/RegistryConfig.java
@@ -8,6 +8,10 @@ public class RegistryConfig {
     public static final int BEAT_TIMEOUT = 30;
     public static final int DEAD_TIMEOUT = BEAT_TIMEOUT * 3;
 
+
+    public static final int DEAD_TIMEOUT_TIME = BEAT_TIMEOUT * 3;
+
+
     public enum RegistType{ EXECUTOR, ADMIN }
 
 }
