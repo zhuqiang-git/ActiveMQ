121822f0394111141ac500cc203030eb9af96a37
qiang <61818074+zhuqiang-git@users.noreply.github.com>
2024-12-12 16:25:14 +0800

    异步删除指定 ZSet 中的指定 memberName 元素

--- src/main/java/com/fastbj/activeMQ/Queue/JmsReceiver.java
+++ src/main/java/com/fastbj/activeMQ/Queue/JmsReceiver.java
@@ -30,5 +30,38 @@ public class JmsReceiver {
         }
     }
 
+
+    
+ 
+    /**
+     * 异步删除指定 ZSet 中的指定 memberName 元素
+     */
+    public void removeZSetMemberAsync(String key, String memberName) {
+        RScoredSortedSet<Object> scoredSortedSet = client.getScoredSortedSet(key);
+        if (!scoredSortedSet.isExists()) {
+            return;
+        }
+        scoredSortedSet.removeAsync(memberName);
+    }
+ 
+ 
+    /**
+     * 异步批量删除指定 ZSet 中的指定 member 元素列表
+     */
+    public void removeZSetMemberAsync(String key, List<String> memberList) {
+        RScoredSortedSet<Object> scoredSortedSet = client.getScoredSortedSet(key);
+        if (!scoredSortedSet.isExists()) {
+            return;
+        }
+        RBatch batch = client.createBatch();
+        memberList.forEach(member -> batch.getScoredSortedSet(key).removeAsync(member));
+        batch.execute();
+    }
+
+
+
+
+    
+
     
 }
