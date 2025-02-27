b86d7b94598aa36bcf9b1f8cac1a75df33b0650e
qiang <61818074+zhuqiang-git@users.noreply.github.com>
2025-02-27 08:26:14 +0800

    解决高并发下System.currentTimeMillis卡顿

--- src/main/java/com/fastbj/activeMQ/Queue/BusinessAnalysis.java
+++ src/main/java/com/fastbj/activeMQ/Queue/BusinessAnalysis.java
@@ -10,4 +10,34 @@ public class BusinessAnalysis {
         
         System.out.println("ok");
     }
+
+
+    
+    private final int period;
+
+    private final AtomicLong now;
+
+    private static class InstanceHolder {
+        private static final SystemClock INSTANCE = new SystemClock(1);
+    }
+
+    private SystemClock(int period) {
+        this.period = period;
+        this.now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    private static SystemClock instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(runnable -> {
+            Thread thread = new Thread(runnable, "System Clock");
+            thread.setDaemon(true);
+            return thread;
+        });
+        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), period, period, TimeUnit.MILLISECONDS);
+    }
+
 }
