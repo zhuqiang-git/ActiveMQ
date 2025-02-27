d1b63a2e285c62c691e47cc6f26c84a493fb4372
qiang <61818074+zhuqiang-git@users.noreply.github.com>
2025-02-27 08:25:43 +0800

    校验nextWrapper的状态

--- src/main/java/com/fastbj/activeMQ/Topic/TopReceiver.java
+++ src/main/java/com/fastbj/activeMQ/Topic/TopReceiver.java
@@ -47,4 +47,128 @@ public class TopReceiver {
 
 
     }
+
+
+    /**
+     * worker将来要处理的param
+     */
+    private T param;
+    private IWorker<T, V> worker;
+    private ICallback<T, V> callback;
+    /**
+     * 在自己后面的wrapper，如果没有，自己就是末尾；如果有一个，就是串行；如果有多个，有几个就需要开几个线程</p>
+     * -------2
+     * 1
+     * -------3
+     * 如1后面有2、3
+     */
+    private List<WorkerWrapper<?, ?>> nextWrappers;
+    /**
+     * 依赖的wrappers，有2种情况，1:必须依赖的全部完成后，才能执行自己 2:依赖的任何一个、多个完成了，就可以执行自己
+     * 通过must字段来控制是否依赖项必须完成
+     * 1
+     * -------3
+     * 2
+     * 1、2执行完毕后才能执行3
+     */
+    private List<DependWrapper> dependWrappers;
+    /**
+     * 标记该事件是否已经被处理过了，譬如已经超时返回false了，后续rpc又收到返回值了，则不再二次回调
+     * 经试验,volatile并不能保证"同一毫秒"内,多线程对该值的修改和拉取
+     * <p>
+     * 1-finish, 2-error, 3-working
+     */
+    private AtomicInteger state = new AtomicInteger(0);
+    /**
+     * 该map存放所有wrapper的id和wrapper映射
+     */
+    private Map<String, WorkerWrapper> forParamUseWrappers;
+    /**
+     * 也是个钩子变量，用来存临时的结果
+     */
+    private volatile WorkResult<V> workResult = WorkResult.defaultResult();
+    /**
+     * 是否在执行自己前，去校验nextWrapper的执行结果<p>
+     * 1   4
+     * -------3
+     * 2
+     * 如这种在4执行前，可能3已经执行完毕了（被2执行完后触发的），那么4就没必要执行了。
+     * 注意，该属性仅在nextWrapper数量<=1时有效，>1时的情况是不存在的
+     */
+    private volatile boolean needCheckNextWrapperResult = true;
+
+    private static final int FINISH = 1;
+    private static final int ERROR = 2;
+    private static final int WORKING = 3;
+    private static final int INIT = 0;
+
+    private WorkerWrapper(String id, IWorker<T, V> worker, T param, ICallback<T, V> callback) {
+        if (worker == null) {
+            throw new NullPointerException("async.worker is null");
+        }
+        this.worker = worker;
+        this.param = param;
+        this.id = id;
+        //允许不设置回调
+        if (callback == null) {
+            callback = new DefaultCallback<>();
+        }
+        this.callback = callback;
+    }
+
+    /**
+     * 开始工作
+     * fromWrapper代表这次work是由哪个上游wrapper发起的
+     */
+    private void work(ExecutorService executorService, WorkerWrapper fromWrapper, long remainTime, Map<String, WorkerWrapper> forParamUseWrappers) {
+        this.forParamUseWrappers = forParamUseWrappers;
+        //将自己放到所有wrapper的集合里去
+        forParamUseWrappers.put(id, this);
+        long now = SystemClock.now();
+        //总的已经超时了，就快速失败，进行下一个
+        if (remainTime <= 0) {
+            fastFail(INIT, null);
+            beginNext(executorService, now, remainTime);
+            return;
+        }
+        //如果自己已经执行过了。
+        //可能有多个依赖，其中的一个依赖已经执行完了，并且自己也已开始执行或执行完毕。当另一个依赖执行完毕，又进来该方法时，就不重复处理了
+        if (getState() == FINISH || getState() == ERROR) {
+            beginNext(executorService, now, remainTime);
+            return;
+        }
+
+        //如果在执行前需要校验nextWrapper的状态
+        if (needCheckNextWrapperResult) {
+            //如果自己的next链上有已经出结果或已经开始执行的任务了，自己就不用继续了
+            if (!checkNextWrapperResult()) {
+                fastFail(INIT, new SkippedException());
+                beginNext(executorService, now, remainTime);
+                return;
+            }
+        }
+
+        //如果没有任何依赖，说明自己就是第一批要执行的
+        if (dependWrappers == null || dependWrappers.size() == 0) {
+            fire();
+            beginNext(executorService, now, remainTime);
+            return;
+        }
+
+        /*如果有前方依赖，存在两种情况
+         一种是前面只有一个wrapper。即 A  ->  B
+        一种是前面有多个wrapper。A C D ->   B。需要A、C、D都完成了才能轮到B。但是无论是A执行完，还是C执行完，都会去唤醒B。
+        所以需要B来做判断，必须A、C、D都完成，自己才能执行 */
+
+        //只有一个依赖
+        if (dependWrappers.size() == 1) {
+            doDependsOneJob(fromWrapper);
+            beginNext(executorService, now, remainTime);
+        } else {
+            //有多个依赖时
+            doDependsJobs(executorService, dependWrappers, fromWrapper, now, remainTime);
+        }
+
+    }
+
 }
