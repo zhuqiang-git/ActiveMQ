21afeb97d85e1a6252f815119212373a7ef85694
qiang <61818074+zhuqiang-git@users.noreply.github.com>
2025-03-03 16:07:03 +0800

    安全加密处理器

---
+++ src/main/java/com/fastbj/activeMQ/service/SafetyEncryptProcessor.java
@@ -0,0 +1,63 @@
+
+package com.baomidou.mybatisplus.autoconfigure;
+
+import com.baomidou.mybatisplus.core.toolkit.AES;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.OriginTrackedMapPropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+import java.util.HashMap;
+
+/**
+ * 安全加密处理器
+ *
+ * @author hubin
+ * @since 2020-05-23
+ */
+public class SafetyEncryptProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        /**
+         * 命令行中获取密钥
+         */
+        String mpwKey = null;
+        for (PropertySource<?> ps : environment.getPropertySources()) {
+            if (ps instanceof SimpleCommandLinePropertySource) {
+                SimpleCommandLinePropertySource source = (SimpleCommandLinePropertySource) ps;
+                mpwKey = source.getProperty("mpw.key");
+                break;
+            }
+        }
+        /**
+         * 处理加密内容
+         */
+        if (StringUtils.isNotBlank(mpwKey)) {
+            HashMap<String, Object> map = new HashMap<>();
+            for (PropertySource<?> ps : environment.getPropertySources()) {
+                if (ps instanceof OriginTrackedMapPropertySource) {
+                    OriginTrackedMapPropertySource source = (OriginTrackedMapPropertySource) ps;
+                    for (String name : source.getPropertyNames()) {
+                        Object value = source.getProperty(name);
+                        if (value instanceof String) {
+                            String str = (String) value;
+                            if (str.startsWith("mpw:")) {
+                                map.put(name, AES.decrypt(str.substring(4), mpwKey));
+                            }
+                        }
+                    }
+                }
+            }
+            // 将解密的数据放入环境变量，并处于第一优先级上
+            if (CollectionUtils.isNotEmpty(map)) {
+                environment.getPropertySources().addFirst(new MapPropertySource("custom-encrypt", map));
+            }
+        }
+    }
+}
