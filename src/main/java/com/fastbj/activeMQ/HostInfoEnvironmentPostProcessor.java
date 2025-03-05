0fc6cbe31e3dd52d2386265268e267052dbf22bb
qiang <16562083@qq.com>
2025-03-05 17:12:04 +0800

    配置处理前拦截事件处理信息

---
+++ src/main/java/com/fastbj/activeMQ/HostInfoEnvironmentPostProcessor.java
@@ -0,0 +1,27 @@
+
+	// Before ConfigFileApplicationListener
+	private int order = ConfigFileApplicationListener.DEFAULT_ORDER - 1;
+
+	@Override
+	public int getOrder() {
+		return this.order;
+	}
+
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		InetUtils.HostInfo hostInfo = getFirstNonLoopbackHostInfo(environment);
+		LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+		map.put("spring.cloud.client.hostname", hostInfo.getHostname());
+		map.put("spring.cloud.client.ip-address", hostInfo.getIpAddress());
+		MapPropertySource propertySource = new MapPropertySource("springCloudClientHostInfo", map);
+		environment.getPropertySources().addLast(propertySource);
+	}
+
+	private HostInfo getFirstNonLoopbackHostInfo(ConfigurableEnvironment environment) {
+		InetUtilsProperties target = new InetUtilsProperties();
+		ConfigurationPropertySources.attach(environment);
+		Binder.get(environment).bind(InetUtilsProperties.PREFIX, Bindable.ofInstance(target));
+		try (InetUtils utils = new InetUtils(target)) {
+			return utils.findFirstNonLoopbackHostInfo();
+		}
+	}
