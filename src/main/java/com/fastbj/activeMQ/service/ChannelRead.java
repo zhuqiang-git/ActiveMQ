a5b1d2db754b511398f26c63c33f40a5fd5f73e3
qiang <16562083@qq.com>
2025-03-07 17:28:09 +0800

    Not using "valueOf' to avoid static init cost

--- src/main/java/com/fastbj/activeMQ/service/ChannelRead.java
+++ src/main/java/com/fastbj/activeMQ/service/ChannelRead.java
@@ -41,3 +41,38 @@
                 ReferenceCountUtil.release(evt);
             }
         }
+
+
+
+
+	static {
+		// Not using "valueOf' to avoid static init cost
+		ALL = new MediaType("*", "*");
+		APPLICATION_ATOM_XML = new MediaType("application", "atom+xml");
+		APPLICATION_CBOR = new MediaType("application", "cbor");
+		APPLICATION_FORM_URLENCODED = new MediaType("application", "x-www-form-urlencoded");
+		APPLICATION_GRAPHQL = new MediaType("application", "graphql+json");
+		APPLICATION_JSON = new MediaType("application", "json");
+		APPLICATION_JSON_UTF8 = new MediaType("application", "json", StandardCharsets.UTF_8);
+		APPLICATION_NDJSON = new MediaType("application", "x-ndjson");
+		APPLICATION_OCTET_STREAM = new MediaType("application", "octet-stream");
+		APPLICATION_PDF = new MediaType("application", "pdf");
+		APPLICATION_PROBLEM_JSON = new MediaType("application", "problem+json");
+		APPLICATION_PROBLEM_JSON_UTF8 = new MediaType("application", "problem+json", StandardCharsets.UTF_8);
+		APPLICATION_PROBLEM_XML = new MediaType("application", "problem+xml");
+		APPLICATION_RSS_XML = new MediaType("application", "rss+xml");
+		APPLICATION_STREAM_JSON = new MediaType("application", "stream+json");
+		APPLICATION_XHTML_XML = new MediaType("application", "xhtml+xml");
+		APPLICATION_XML = new MediaType("application", "xml");
+		IMAGE_GIF = new MediaType("image", "gif");
+		IMAGE_JPEG = new MediaType("image", "jpeg");
+		IMAGE_PNG = new MediaType("image", "png");
+		MULTIPART_FORM_DATA = new MediaType("multipart", "form-data");
+		MULTIPART_MIXED = new MediaType("multipart", "mixed");
+		MULTIPART_RELATED = new MediaType("multipart", "related");
+		TEXT_EVENT_STREAM = new MediaType("text", "event-stream");
+		TEXT_HTML = new MediaType("text", "html");
+		TEXT_MARKDOWN = new MediaType("text", "markdown");
+		TEXT_PLAIN = new MediaType("text", "plain");
+		TEXT_XML = new MediaType("text", "xml");
+	}
