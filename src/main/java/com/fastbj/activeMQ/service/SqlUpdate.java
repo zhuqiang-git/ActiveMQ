2475a39a294b074801a402ec246aaa3f9b3661a7
qiang <16562083@qq.com>
2025-03-07 08:44:28 +0800

    初始化vm方法

--- src/main/java/com/fastbj/activeMQ/service/SqlUpdate.java
+++ src/main/java/com/fastbj/activeMQ/service/SqlUpdate.java
@@ -33,3 +33,25 @@ public int updateByNamedParam(Map<String, ?> paramMap) throws DataAccessExceptio
 		checkRowsAffected(rowsAffected);
 		return rowsAffected;
 	}
+
+
+ /**
+     * 初始化vm方法
+     */
+    public static void initVelocity()
+    {
+        Properties p = new Properties();
+        try
+        {
+            // 加载classpath目录下的vm文件
+            p.setProperty("resource.loader.file.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            // 定义字符集
+            p.setProperty(Velocity.INPUT_ENCODING, Constants.UTF8);
+            // 初始化Velocity引擎，指定配置Properties
+            Velocity.init(p);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
