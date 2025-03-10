4f1ca552e5f8a59a7e4abbeb620f51d1ada4bb2c
qiang <16562083@qq.com>
2025-03-10 08:24:01 +0800

    Create an instance with the given map of file extensions and media types.

---
+++ src/main/java/com/fastbj/activeMQ/service/AbstractMappingContentNegotiationStrategy.java
@@ -0,0 +1,101 @@
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.context.request.NativeWebRequest;
+
+public abstract class AbstractMappingContentNegotiationStrategy extends MappingMediaTypeFileExtensionResolver
+		implements ContentNegotiationStrategy {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	private boolean useRegisteredExtensionsOnly = false;
+
+	private boolean ignoreUnknownExtensions = false;
+
+
+	public AbstractMappingContentNegotiationStrategy(@Nullable Map<String, MediaType> mediaTypes) {
+		super(mediaTypes);
+	}
+
+
+	public void setUseRegisteredExtensionsOnly(boolean useRegisteredExtensionsOnly) {
+		this.useRegisteredExtensionsOnly = useRegisteredExtensionsOnly;
+	}
+
+	public boolean isUseRegisteredExtensionsOnly() {
+		return this.useRegisteredExtensionsOnly;
+	}
+
+
+	public void setIgnoreUnknownExtensions(boolean ignoreUnknownExtensions) {
+		this.ignoreUnknownExtensions = ignoreUnknownExtensions;
+	}
+
+	public boolean isIgnoreUnknownExtensions() {
+		return this.ignoreUnknownExtensions;
+	}
+
+
+	@Override
+	public List<MediaType> resolveMediaTypes(NativeWebRequest webRequest)
+			throws HttpMediaTypeNotAcceptableException {
+
+		return resolveMediaTypeKey(webRequest, getMediaTypeKey(webRequest));
+	}
+
+
+	public List<MediaType> resolveMediaTypeKey(NativeWebRequest webRequest, @Nullable String key)
+			throws HttpMediaTypeNotAcceptableException {
+
+		if (StringUtils.hasText(key)) {
+			MediaType mediaType = lookupMediaType(key);
+			if (mediaType != null) {
+				handleMatch(key, mediaType);
+				return Collections.singletonList(mediaType);
+			}
+			mediaType = handleNoMatch(webRequest, key);
+			if (mediaType != null) {
+				addMapping(key, mediaType);
+				return Collections.singletonList(mediaType);
+			}
+		}
+		return MEDIA_TYPE_ALL_LIST;
+	}
+
+
+	@Nullable
+	protected abstract String getMediaTypeKey(NativeWebRequest request);
+
+
+	protected void handleMatch(String key, MediaType mediaType) {
+	}
+
+
+	@Nullable
+	protected MediaType handleNoMatch(NativeWebRequest request, String key)
+			throws HttpMediaTypeNotAcceptableException {
+
+		if (!isUseRegisteredExtensionsOnly()) {
+			Optional<MediaType> mediaType = MediaTypeFactory.getMediaType("file." + key);
+			if (mediaType.isPresent()) {
+				return mediaType.get();
+			}
+		}
+		if (isIgnoreUnknownExtensions()) {
+			return null;
+		}
+		throw new HttpMediaTypeNotAcceptableException(getAllMediaTypes());
+	}
+
+}
