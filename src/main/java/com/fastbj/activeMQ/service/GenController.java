e8c611d50e5290a811be0541a4017c02ef8d2a1e
qiang <16562083@qq.com>
2025-03-10 08:25:57 +0800

    修改保存代码生成业务

--- src/main/java/com/fastbj/activeMQ/service/GenController.java
+++ src/main/java/com/fastbj/activeMQ/service/GenController.java
@@ -214,6 +214,32 @@ public class GenController extends BaseController
         }
     }
 
+
+    private <T extends ObjectError> T escapeObjectError(@Nullable T source) {
+		if (source == null) {
+			return null;
+		}
+		String defaultMessage = source.getDefaultMessage();
+		if (defaultMessage != null) {
+			defaultMessage = HtmlUtils.htmlEscape(defaultMessage);
+		}
+		if (source instanceof FieldError) {
+			FieldError fieldError = (FieldError) source;
+			Object value = fieldError.getRejectedValue();
+			if (value instanceof String) {
+				value = HtmlUtils.htmlEscape((String) value);
+			}
+			return (T) new FieldError(
+					fieldError.getObjectName(), fieldError.getField(), value, fieldError.isBindingFailure(),
+					fieldError.getCodes(), fieldError.getArguments(), defaultMessage);
+		}
+		else {
+			return (T) new ObjectError(
+					source.getObjectName(), source.getCodes(), source.getArguments(), defaultMessage);
+		}
+	}
+
+
     /**
      * 修改保存代码生成业务
      */
