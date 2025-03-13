c8f407c6e6a5a016fb28d965958eaefaaa0e5e4a
qiang <16562083@qq.com>
2025-03-13 08:13:49 +0800

    获取登录部门id

--- src/main/java/com/fastbj/activeMQ/service/Anonymous.java
+++ src/main/java/com/fastbj/activeMQ/service/Anonymous.java
@@ -16,4 +16,44 @@ import java.lang.annotation.Target;
 @Documented
 public @interface Anonymous
 {
+  
+    /**
+     * 页面跳转
+     */
+    public String redirect(String url)
+    {
+        return StringUtils.format("redirect:{}", url);
+    }
+
+    /**
+     * 获取用户缓存信息
+     */
+    public LoginUser getLoginUser()
+    {
+        return SecurityUtils.getLoginUser();
+    }
+
+    /**
+     * 获取登录用户id
+     */
+    public Long getUserId()
+    {
+        return getLoginUser().getUserId();
+    }
+
+    /**
+     * 获取登录部门id
+     */
+    public Long getDeptId()
+    {
+        return getLoginUser().getDeptId();
+    }
+
+    /**
+     * 获取登录用户名
+     */
+    public String getUsername()
+    {
+        return getLoginUser().getUsername();
+    }
 }
