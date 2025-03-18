8b62d5f9ec4d5864f0469f889a0d2d6398b4bc0c
qiang <16562083@qq.com>
2025-03-18 09:01:47 +0800

    设置模板变量信息

--- src/main/java/com/fastbj/activeMQ/service/SWTFXUtils.java
+++ src/main/java/com/fastbj/activeMQ/service/SWTFXUtils.java
@@ -159,7 +159,60 @@ public class SWTFXUtils {
     }
 
 
+ private final byte[] body;
 
+    public RepeatedlyRequestWrapper(HttpServletRequest request, ServletResponse response) throws IOException
+    {
+        super(request);
+        request.setCharacterEncoding(Constants.UTF8);
+        response.setCharacterEncoding(Constants.UTF8);
+
+        body = HttpHelper.getBodyString(request).getBytes(Constants.UTF8);
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException
+    {
+        return new BufferedReader(new InputStreamReader(getInputStream()));
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException
+    {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(body);
+        return new ServletInputStream()
+        {
+            @Override
+            public int read() throws IOException
+            {
+                return bais.read();
+            }
+
+            @Override
+            public int available() throws IOException
+            {
+                return body.length;
+            }
+
+            @Override
+            public boolean isFinished()
+            {
+                return false;
+            }
+
+            @Override
+            public boolean isReady()
+            {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener)
+            {
+
+            }
+        };
+    }
       /** 项目空间路径 */
     private static final String PROJECT_PATH = "main/java";
 
