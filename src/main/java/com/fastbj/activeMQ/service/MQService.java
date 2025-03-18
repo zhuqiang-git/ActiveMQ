0a51150d982f309486a57aa8394500993ec63f64
qiang <16562083@qq.com>
2025-03-18 09:01:18 +0800

    通过ID查询配置

--- src/main/java/com/fastbj/activeMQ/service/MQService.java
+++ src/main/java/com/fastbj/activeMQ/service/MQService.java
@@ -153,7 +153,37 @@ public class MQService implements ApplicationListener<SecurityConnectEvent>, Act
     }
 
 
+  @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
 
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+    {
+        ServletRequest requestWrapper = null;
+        if (request instanceof HttpServletRequest
+                && StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_JSON_VALUE))
+        {
+            requestWrapper = new RepeatedlyRequestWrapper((HttpServletRequest) request, response);
+        }
+        if (null == requestWrapper)
+        {
+            chain.doFilter(request, response);
+        }
+        else
+        {
+            chain.doFilter(requestWrapper, response);
+        }
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
 
     /**
      * 查询参数配置信息
