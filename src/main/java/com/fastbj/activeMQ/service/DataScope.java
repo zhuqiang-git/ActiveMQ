923eb326263f238d7e1a6e4949eb8704e154d663
linux <16562083@qq.com>
2025-04-09 17:20:05 +0800

    部门表的别名

--- src/main/java/com/fastbj/activeMQ/service/DataScope.java
+++ src/main/java/com/fastbj/activeMQ/service/DataScope.java
@@ -1,10 +1,15 @@
 package com.ruoyi.common.annotation;
 
+import java.io.File;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 数据权限过滤注解
@@ -16,6 +21,48 @@ import java.lang.annotation.Target;
 @Documented
 public @interface DataScope
 {
+
+
+    private static final String MODE_PROPERTY_KEY_STAND_MODE = "nacos.mode";
+
+    private static final String MODE_PROPERTY_KEY_FUNCTION_MODE = "nacos.function.mode";
+
+    private static final String NACOS_MODE_STAND_ALONE = "stand alone";
+
+    private static final String DEFAULT_FUNCTION_MODE = "All";
+
+    private static final String LOCAL_IP_PROPERTY_KEY = "nacos.local.ip";
+
+    private static final String NACOS_APPLICATION_CONF = "nacos_application_conf";
+
+    private static final Map<String, Object> SOURCES = new ConcurrentHashMap<>();
+
+    private boolean isConsoleDeploymentType;
+
+    public NacosConsoleStartUp() {
+        super(NacosStartUp.CONSOLE_START_UP_PHASE);
+    }
+
+    @Override
+    protected String getPhaseNameInStartingInfo() {
+        return "Nacos Console";
+    }
+
+    @Override
+    public String[] makeWorkDir() {
+        isConsoleDeploymentType = Constants.NACOS_DEPLOYMENT_TYPE_CONSOLE.equals(
+                System.getProperty(Constants.NACOS_DEPLOYMENT_TYPE));
+        if (isConsoleDeploymentType) {
+            try {
+                Path path = Paths.get(EnvUtil.getNacosHome(), "logs");
+                DiskUtils.forceMkdir(new File(path.toUri()));
+            } catch (Exception e) {
+                throw new NacosRuntimeException(ErrorCode.IOMakeDirError.getCode(), e);
+            }
+            return new String[] {EnvUtil.getNacosHome() + File.pathSeparator + "logs"};
+        }
+        return super.makeWorkDir();
+    }
     /**
      * 部门表的别名
      */
